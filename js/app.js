02c6fdaf49f02eee8ff2b6c86b02f7dd0d6a28af
chenmu5241 <chenmu5241@163.com>
2018-06-24 16:10:10 +0800

    解决bug

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 (function(window, mui) {
-	window.basePath = "http://192.168.1.154:8080/fz-app/";
-	window.baseUpload = "http://192.168.1.154:8181/fz-upload/"; //全局记录服务器端地址
-/*	window.basePath = "http://192.168.2.100:8080/fz-app/";
-	window.baseUpload = "http://192.168.2.100:8080/fz-upload/"; */
+/*	window.basePath = "http://192.168.1.154:8080/fz-app/";
+	window.baseUpload = "http://192.168.1.154:8181/fz-upload/"; //全局记录服务器端地址*/
+	window.basePath = "http://192.168.2.100:8080/fz-app/";
+	window.baseUpload = "http://192.168.2.100:8181/fz-upload/"; 
 
 	//封装localStorage
 	window.myLocalStorage = {};
